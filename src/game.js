104d04acb24035dd2e9f48b9560704dab2de63a4
Zack <zack.musicteacher@gamil.com>
2020-05-25 14:22:39 -0700

    Door and key rendering

--- src/game.js
+++ src/game.js
@@ -8,10 +8,11 @@ class Game {
         Game.DIM_Y = 700;
         Game.MAP_EL_WIDTH = 50;
         Game.MAP_EL_HEIGHT = 50;
-        this.hazrds = ['a']
+        this.hazards = ['a']
         this.ground_color = "#000000"
         this.keysPressed = keysPressed;
         this.levelNum = 0;
+        this.hasKey = false;
         this.level = Levels[this.levelNum].level;
         this.levelType = Levels[this.levelNum].type;
         this.hazardType = Levels[this.levelNum].hazardType
@@ -27,6 +28,7 @@ class Game {
     }
 
     drawLevel(ctx) {
+        ctx.clearRect(0, 0, Game.DIM_X, Game.DIM_Y)
         for (let i = 0; i < this.level.length; i ++) {
             for (let j = 0; j < this.level[i].length; j ++) {
                 let currentBlock = this.level[i][j];
@@ -35,8 +37,22 @@ class Game {
                     let platformXPos = j * Game.MAP_EL_WIDTH;
                     let platformYPos = i * Game.MAP_EL_HEIGHT;
 
-                    if (this.hazrds.includes(currentBlock)) {
+                    if (this.hazards.includes(currentBlock)) {
                         img.src = `./assets/Tiles_resized/resized_liquid${this.hazardType}${this.spriteFilenames[this.level[i][j]]}.png`
+                    } else if (currentBlock === 'k') {
+                        if (!this.hasKey) {
+                            // draw key
+                            img.src = `./assets/Items/keyRed.png`
+                        } else {
+                            continue
+                        }
+                    } else if (currentBlock === 'd' || currentBlock === 'o') {
+                        let doorStatus = 'closed'
+                        let doorPiece = currentBlock === 'd' ? 'Mid' : 'Top'
+                        if (this.hasKey) {
+                            doorStatus = 'open'
+                        } 
+                        img.src = `./assets/Tiles_resized/resized_door_${doorStatus}${doorPiece}.png`
                     } else {
                         img.src = `./assets/Tiles_resized/resized_${this.levelType}${this.spriteFilenames[this.level[i][j]]}.png`;
                     }
@@ -63,7 +79,9 @@ class Game {
     }
     
     resetLevel() {
-
+        // Reset player position
+        this.player.posX = Levels[this.levelNum].startPos[0];
+        this.player.posY = Levels[this.levelNum].startPos[1];
     }
     
     step(ctx) {
@@ -71,7 +89,8 @@ class Game {
 
         for (let i = 0; i < Game.DIM_Y / Game.MAP_EL_HEIGHT; i ++) {
             for (let j = 0; j < Game.DIM_X / Game.MAP_EL_WIDTH; j ++) {
-                if (this.level[i][j] != 0) {
+                let currentBlock = this.level[i][j]
+                if (currentBlock != 0) {
                     let platformXPos = j * Game.MAP_EL_WIDTH;
                     let platformYPos = i * Game.MAP_EL_HEIGHT;
                     let mapEl = {
@@ -81,7 +100,16 @@ class Game {
                         width: Game.MAP_EL_HEIGHT
                     }
                     if (this.checkCollisions(this.player,mapEl)) {
-                        this.player.resolveMapCollision(mapEl)
+                        if (currentBlock === 'k') {
+                            this.hasKey = true;
+                            console.log(this.hasKey)
+                            this.drawLevel(ctx)
+                        } else if (!this.hazards.includes(currentBlock)) {
+                            this.player.resolveMapCollision(mapEl)
+
+                        } else {
+                            this.resetLevel()
+                        }
                     }
                 }
             }
